52875b0ec56ff6a8bf952ac8aa670012d5023572
Victor Nitu <nitu.v.r@gmail.com>
2020-06-09 15:06:06 +0200

    increment version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='phisuite.data',  # Required
-    version='0.0.2',  # Required
+    version='0.0.3',  # Required
     description='Phi Suite Data',  # Optional
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',  # Optional
